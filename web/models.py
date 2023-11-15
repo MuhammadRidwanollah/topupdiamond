17b3fa2e4ce19304cb314e17eef227633d04ff35
wannn <rikk@gmail.com>
2023-11-15 13:21:58 +0700

    perubahan

--- web/models.py
+++ web/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    server = models.CharField(max_length=255)
+    player_id = models.IntegerField()
+    nickname = models.CharField(max_length=255)
+
+class Order(models.Model):
+    total_diamond = models.IntegerField()
+    price = models.IntegerField()
+    payment_method = models.CharField(max_length=255, default="DANA")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
